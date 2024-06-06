7dc6d0ecf21337abbbe177ce22ebe4ecbcd6de06
locjdev <voduyloc.dev@gmail.com>
2024-06-07 06:32:56 +0700

    update exercise2

--- src/main/java/exercises/Exercies2.java
+++ src/main/java/exercises/Exercies2.java
@@ -3,7 +3,16 @@ package exercises;
 public class Exercies2 {
     public static void main(String[] args) {
         // Question-01
+        System.out.println("Question-01");
         int a = 5;
-        System.out.printf("");
+        System.out.printf("a = %d\n",a);
+        System.out.println("--------------------");
+
+        // Question-01
+        System.out.println("Question-02");
+        int b = 100000000;
+        System.out.printf("b = %,d\n",b);
+        System.out.println("--------------------");
+
     }
 }
