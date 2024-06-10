6b81466f608542fd1bcdaa82f65709fa2189ee92
locjdev <voduyloc.dev@gmail.com>
2024-06-10 22:39:02 +0700

    finish exercise 4

--- src/main/java/Exercise04.java
+++ src/main/java/Exercise04.java
@@ -68,4 +68,99 @@ public class Exercise04 {
         System.out.println("Tên đệm là: " + middleName);
         System.out.println("Tên là: " + firstName);
     }
+
+    void question7() {
+        System.out.println("Question-07");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập họ và tên:");
+        String fullName = scanner.nextLine();
+
+        String[] words = fullName.split(" ");
+        fullName = "";
+        for (String word : words) {
+            if (!word.isEmpty())
+                fullName += Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase() + " ";
+        }
+        fullName = fullName.trim();
+        System.out.println("fullName = " + fullName);
+    }
+
+    void question10(String s1, String s2) {
+        System.out.println("Question-10");
+        if (s1.length() != s2.length()) {
+            System.out.println("2 chuỗi không đối xứng");
+            return;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(s2.length() - 1 - i)) {
+                System.out.println("2 chuỗi không đối xứng");
+                return;
+            }
+        }
+        System.out.println("2 chuỗi đối xứng");
+    }
+
+    void question11(String s1) {
+        System.out.println("Question-11");
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == 'a') count++;
+        }
+        System.out.println("Số kí tự a: " + count);
+    }
+
+    void question12(String s1) {
+        System.out.println("Question-12");
+        String result = "";
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            result += s1.charAt(i);
+        }
+        System.out.println("Đảo ngược của " + s1 + " là: " + result);
+    }
+
+    void question13(String s1) {
+        System.out.println("Question-13");
+        if (s1 == null) {
+            System.out.println("false");
+            return;
+        } else for (int i = 0; i < s1.length(); i++) {
+            if (Character.isDigit(s1.charAt(i))) {
+                System.out.println("false");
+                return;
+            }
+
+        }
+        System.out.println("true");
+    }
+
+    void question14(String s1, char oldChar, char newChar) {
+        System.out.println("Question-14");
+        System.out.println("s1 = " + s1.replace(oldChar, newChar));
+    }
+
+    void question15(String s1) {
+        System.out.println("Question-15");
+        String[] words = s1.split(" ");
+        String s2 = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (!words[i].isEmpty())
+                s2 += words[i] + " ";
+        }
+        System.out.println("s = " + s2.trim());
+    }
+
+    void question16(String s1, int n) {
+        System.out.println("Question-16");
+        if (s1.length() < n)
+            System.out.println("KO");
+        else {
+            int length = s1.length();
+            while (length >= n) {
+                System.out.println("[" + s1.substring(0, n) + "]");
+                length -= n;
+                s1 = s1.substring(n, s1.length());
+            }
+        }
+        System.out.println("[" + s1 + "]");
+    }
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -8,6 +8,13 @@ public class Program {
 //        exercise04.question4();
 //        exercise04.question5();
 //        exercise04.question6();
-
+//        exercise04.question7();
+//        exercise04.question10("work","krow");
+//        exercise04.question11("Java Core");
+//        exercise04.question12("Java Core");
+//        exercise04.question13("Java Core");
+//        exercise04.question14("Java Core",'a','*');
+//        exercise04.question15("  I    am     developer  ");
+        exercise04.question16("123 456 789 10",3);
     }
 }
