d4fdd9eaa36e9378ca863ba5ee797dfe94763188
locjdev <voduyloc.dev@gmail.com>
2024-06-22 19:13:31 +0700

    update

---
+++ src/main/java/exercise04/Salary.java
@@ -0,0 +1,13 @@
+package exercise04;
+
+public class Salary<N extends Number> {
+    private N value;
+
+    public void showInfor() {
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
